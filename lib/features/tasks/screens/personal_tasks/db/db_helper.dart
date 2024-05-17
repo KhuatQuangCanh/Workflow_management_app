6faad94ca08befe4884ffab03fba5dbece9838cf
KhuatQuangCanh <kqc07062002@gmail.com>
2024-05-18 03:30:45 +0700

    edit static update int=> int? and and if(is!=null)

--- lib/features/tasks/screens/personal_tasks/db/db_helper.dart
+++ lib/features/tasks/screens/personal_tasks/db/db_helper.dart
@@ -48,11 +48,14 @@ class DBHelper {
    return await _db!.delete(_tableName, where: 'id=?', whereArgs: [task.id]);
   }
 
-  static update(int id) async {
-    return await _db!.rawUpdate('''
+  static update(int? id) async {
+    if(id !=null){
+      return await _db!.rawUpdate('''
     UPDATE tasks
     SET isCompleted = ?
     WHERE id =?
     ''',[1,id]);
+    }
+
   }
 }
